a36475fde26dcece69d3f98c17ddbad377d390d6
yxc <286464263@qq.com>
2017-11-15 00:36:06 +0800

    db connect

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var user = require('./public/users');
 
 app.use(express.static('public')); //静态资源的入口
 app.use('/user',user);
+
+//http://blog.csdn.net/zhuming3834/article/details/54691317
 // app.use('/',function (req, res) {
 //     res.sendfile(__dirname + "/" + "views/index.html" );
 // });
+
 app.all('/', function(req, res, next){
     res.sendfile(__dirname + "/" + "views/index.html" );
 });
